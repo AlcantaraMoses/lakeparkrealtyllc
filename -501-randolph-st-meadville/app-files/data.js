f0a2c654c580e06844d5069b57959ae9c4d760f2
ClickThrill <moses.alcantara15@gmail.com>
2022-01-25 20:44:47 -0500

    update 501

---
+++ -501-randolph-st-meadville/app-files/data.js
@@ -0,0 +1,200 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-livingroom",
+      "name": "Livingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.2447465066371395,
+        "pitch": -0.007478729593533018,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.13918236717763577,
+        "pitch": -0.0320811225256481,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22619444456731586,
+          "pitch": 0.14937629586931855,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.22552389362149583,
+        "pitch": 0.07046612643950034,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1311543472259196,
+          "pitch": 0.03776298681598078,
+          "rotation": 0,
+          "target": "1-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-livingroom2",
+      "name": "Livingroom2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.4174144085189262,
+        "pitch": -0.125125247287734,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17063800374434734,
+          "pitch": 0.19059869058144585,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": -2.2491934556804853,
+          "pitch": 0.10213185374841238,
+          "rotation": 0,
+          "target": "0-livingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom2",
+      "name": "Bathroom2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.686950521564091,
+        "pitch": 0.0030082287039725486,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4664791738187013,
+          "pitch": 0.1186702277930145,
+          "rotation": 0,
+          "target": "1-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": " 501 Randolph St Meadville",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
