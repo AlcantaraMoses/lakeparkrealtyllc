ca1016d40b9c80d57ca68920e5b7f74fe1cf4de8
ClickThrill <moses.alcantara15@gmail.com>
2022-01-27 21:35:04 -0500

    water street

---
+++ water-st-conneaut-lake-pa/app-files/data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.30442219956646,
+        "pitch": 0.036502779691145903,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.744607270453126,
+          "pitch": 0.12318062844394717,
+          "rotation": 0,
+          "target": "1-living-room2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room2",
+      "name": "Living Room2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.9090694922372187,
+        "pitch": -0.09819774102399137,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8490571186464049,
+          "pitch": 0.07606657011496587,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.34022537086393534,
+          "pitch": 0.031016930130777,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5760901994778926,
+        "pitch": -0.019309833805161603,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38184837083838374,
+          "pitch": 0.016691071017909564,
+          "rotation": 0,
+          "target": "1-living-room2"
+        },
+        {
+          "yaw": -1.1221963456396988,
+          "pitch": 0.05580117141506413,
+          "rotation": 0,
+          "target": "3-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1849612305214094,
+        "pitch": 0.03849289029222547,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45336180989380814,
+          "pitch": -0.018612469237456963,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Water St Conneaut Lake Pa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
