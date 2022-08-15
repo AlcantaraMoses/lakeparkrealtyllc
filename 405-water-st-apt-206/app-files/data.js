1214ab4ea3d0efc0ace426a01baedc0dff4f1d6f
ClickThrill <moses.alcantara15@gmail.com>
2022-08-14 21:44:59 -0400

    apt 206

---
+++ 405-water-st-apt-206/app-files/data.js
@@ -0,0 +1,292 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.704469786547346,
+        "pitch": 0.04923690103977485,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.396678583054232,
+          "pitch": -0.17092808657041658,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": -1.5699860168698017,
+          "pitch": -0.17181341713773612,
+          "rotation": 0,
+          "target": "5-bedroom-2"
+        },
+        {
+          "yaw": -1.6757853065013535,
+          "pitch": 0.3907546763431391,
+          "rotation": 0,
+          "target": "3-living-room-part-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.835587812977119,
+        "pitch": 0.06345967191694157,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.706097718327145,
+          "pitch": 0.032910723382553186,
+          "rotation": 0,
+          "target": "4-living-room-part3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-room-part-2",
+      "name": "Living Room Part 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.6329269718322301,
+        "pitch": 0.04729215965754108,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7224057152214387,
+          "pitch": -0.17604565307508402,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": 1.9489785912637227,
+          "pitch": 0.09632600847046291,
+          "rotation": 0,
+          "target": "4-living-room-part3"
+        },
+        {
+          "yaw": -2.114381848701484,
+          "pitch": -0.32876243145814676,
+          "rotation": 0,
+          "target": "5-bedroom-2"
+        },
+        {
+          "yaw": 0.7989985013319263,
+          "pitch": 0.10614072846889222,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 1.4002828710615844,
+          "pitch": -0.0390027974364191,
+          "rotation": 0,
+          "target": "1-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-living-room-part3",
+      "name": "Living Room Part3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.3342253107966346,
+        "pitch": -0.049438002597886666,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7248753340835474,
+          "pitch": -0.08936991121600713,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": 1.694905793257977,
+          "pitch": -0.03193038895179434,
+          "rotation": 0,
+          "target": "5-bedroom-2"
+        },
+        {
+          "yaw": 2.821400352235523,
+          "pitch": -0.016937253628688254,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 2.1437424849337328,
+          "pitch": 0.07567297176255039,
+          "rotation": 0,
+          "target": "3-living-room-part-2"
+        },
+        {
+          "yaw": -2.190522662058143,
+          "pitch": 0.028611983842791844,
+          "rotation": 0,
+          "target": "1-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8226247754104072,
+        "pitch": 0.04443164358787399,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8495533231299657,
+          "pitch": 0.007120450648955057,
+          "rotation": 0,
+          "target": "3-living-room-part-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "405 water st apt 206",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
