fa2564bb62d951e33959977304fbe63987c97f55
ClickThrill <moses.alcantara15@gmail.com>
2022-09-06 23:05:54 -0400

    apt 2

---
+++ apt-2-conneautville/app-files/data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.050374218932415,
+        "pitch": 0.0702775827397275,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6688796782607085,
+          "pitch": 0.17702638467426013,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room2",
+      "name": "Living room2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.606703058594338,
+        "pitch": 0.00877544220814741,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0001020255593343,
+          "pitch": 0.10925240289381577,
+          "rotation": 0,
+          "target": "2-bedroom"
+        },
+        {
+          "yaw": 0.7276932589206719,
+          "pitch": 0.1748413018472874,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9666438934122255,
+        "pitch": 0.08128688310922705,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.792645919210992,
+          "pitch": 0.0502465299791961,
+          "rotation": 0,
+          "target": "1-living-room2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9684746583619024,
+        "pitch": 0.11578159257413034,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1603949175046147,
+          "pitch": 0.17701993573745867,
+          "rotation": 0,
+          "target": "0-bathroom"
+        },
+        {
+          "yaw": 2.1861310565807237,
+          "pitch": 0.09522550055130452,
+          "rotation": 0,
+          "target": "1-living-room2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Apt 2, Conneautville",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
