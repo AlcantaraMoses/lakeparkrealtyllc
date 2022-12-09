ef4dfd8c3e62287321fa6ebdb040184e5b497056
ClickThrill <moses.alcantara15@gmail.com>
2022-12-08 19:59:29 -0500

    push

---
+++ 1000-main-st-apt1/app-files/data.js
@@ -0,0 +1,225 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.2386038459479103,
+        "pitch": 0.06020012636802008,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1796214927803526,
+          "pitch": 0.09697276362724949,
+          "rotation": 0,
+          "target": "3-front-door"
+        },
+        {
+          "yaw": -0.6368043018276985,
+          "pitch": 0.02805827158667995,
+          "rotation": 0,
+          "target": "2-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.387295341019625,
+        "pitch": -0.11597470058878478,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.288451032770455,
+          "pitch": 0.0075069302753583145,
+          "rotation": 0,
+          "target": "2-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.5972257501218614,
+        "pitch": 0.1379437401452197,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.097128148171638,
+          "pitch": 0.10054043605313012,
+          "rotation": 0,
+          "target": "0-kitchen"
+        },
+        {
+          "yaw": 2.8998463378007795,
+          "pitch": 0.06358595626110386,
+          "rotation": 0,
+          "target": "1-bedroom"
+        },
+        {
+          "yaw": -2.7636221044863394,
+          "pitch": -0.0004503649250189312,
+          "rotation": 0,
+          "target": "4-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-front-door",
+      "name": "Front Door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.22335332385661388,
+        "pitch": 0.13272736704129073,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.34820836463243765,
+          "pitch": 0.07160554323640334,
+          "rotation": 0,
+          "target": "0-kitchen"
+        },
+        {
+          "yaw": -1.1589850820447758,
+          "pitch": 0.03751618471788021,
+          "rotation": 0,
+          "target": "2-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.123489063777786,
+        "pitch": 0.25561751788804976,
+        "fov": 1.4665669395098586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9654559998422183,
+          "pitch": -0.01727202648296,
+          "rotation": 0,
+          "target": "2-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "1000 Main st Apt1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
