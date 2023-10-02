6365538732085f0f3d44cefa954f17c9e6c37a6d
ClickThrill <moses.alcantara15@gmail.com>
2023-10-01 21:20:03 -0400

    old st

---
+++ 10246-old-st-rd-apt4/app-files/data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front",
+      "name": "front",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4791989724239656,
+        "pitch": 0.28803661502938027,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5339489380445297,
+          "pitch": -0.18708316999038566,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.266487007659771,
+        "pitch": 0.5083069975745484,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0957196209638598,
+          "pitch": 0.18324361252643584,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.3112522707469125,
+        "pitch": 0.41612463973560665,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.603038519474757,
+          "pitch": 0.4658594040277073,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-stairs",
+      "name": "stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.527514711211353,
+        "pitch": 0.4123254348162497,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9893277329325159,
+          "pitch": 0.5074354714572316,
+          "rotation": 0,
+          "target": "5-bedroom1"
+        },
+        {
+          "yaw": -0.35405772563765936,
+          "pitch": 0.3813871236973121,
+          "rotation": 0,
+          "target": "4-bedroom2"
+        },
+        {
+          "yaw": -1.6662924647222184,
+          "pitch": 0.6097870483853374,
+          "rotation": 0,
+          "target": "0-front"
+        },
+        {
+          "yaw": -2.057047676844528,
+          "pitch": 0.24443229089532714,
+          "rotation": 0,
+          "target": "2-bathroom"
+        },
+        {
+          "yaw": -2.6548933098503067,
+          "pitch": 0.14381268932696223,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": 2.9544876089587158,
+          "pitch": 0.20063894316347408,
+          "rotation": 0,
+          "target": "6-livingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom2",
+      "name": "bedroom2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7350798080346728,
+          "pitch": 0.36328881903409993,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom1",
+      "name": "bedroom1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.652661524914338,
+          "pitch": 0.4016450753369796,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-livingroom",
+      "name": "livingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5971529632825714,
+          "pitch": 0.2180081083744252,
+          "rotation": 0,
+          "target": "3-stairs"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "10246 old st rd apt4",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
