003b00e96638c79d2cd1cf494da2fe0b27f40b44
ClickThrill <moses.alcantara15@gmail.com>
2023-11-07 19:14:26 -0500

    apt 203

---
+++ water-st-apt-203/app-files/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4626331382087585,
+        "pitch": 0.09868131772298305,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6305291934097497,
+          "pitch": 0.2221181170862998,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-livingroom",
+      "name": "livingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7708099257090097,
+        "pitch": 0.14762498706191352,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0834700352023923,
+          "pitch": 0.15064985280923437,
+          "rotation": 0,
+          "target": "2-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.2760431699021488,
+        "pitch": 0.3119252854256711,
+        "fov": 1.5486176214610408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4026940727017205,
+          "pitch": 0.17862170414809242,
+          "rotation": 0,
+          "target": "1-livingroom"
+        },
+        {
+          "yaw": -1.8362026003153122,
+          "pitch": 0.3522891674803681,
+          "rotation": 0,
+          "target": "0-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Water st Apt 203",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
